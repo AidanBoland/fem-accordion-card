48d7c7b2fbeaff44afbba6f2bd13201113fc65fa
Aidan Boland <aidanboland1@icloud.com>
2022-11-10 10:13:36 +0000

    more work on dropdown

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import './Card.scss';
 import '../../index.scss';
 
+import Dropdown from "../Dropdown/Dropdown";
+
 function Card() {
     return (
-        <div className="cardMain"></div>
+        <div className="cardMain"><Dropdown title='test lol' content='awgerhtjrsyaneh'  /></div>
     )
 }
 
