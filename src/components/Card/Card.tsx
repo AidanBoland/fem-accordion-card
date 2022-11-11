670cc31a5c3ebfddf769976fe2792bf342277b2d
Aidan Boland <aidanboland1@icloud.com>
2022-11-11 00:00:25 +0000

    formatting

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React from 'react';
 import './Card.scss';
 import '../../index.scss';
 
-import Dropdown from "../Dropdown/Dropdown";
+import Dropdown from '../Dropdown/Dropdown';
 
 function Card() {
     return (
-        <div className="cardMain"><div className="MainTitle"><h2>FAQ </h2></div><Dropdown title='How many team members can I invite?' content='Teams are currently capped at 50 members, but we are working on providing more options!'  /><div className="SVGs"></div></div>
-    )
+        <div className="cardMain">
+            <div className="MainTitle">
+                <h2>FAQ </h2>
+            </div>
+            <Dropdown
+                title="How many team members can I invite?"
+                content="Teams are currently capped at 50 members, but we are working on providing more options!"
+            />
+            <div className="SVGs"></div>
+        </div>
+    );
 }
 
 export default Card;
