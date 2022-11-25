b8266264311fea535bfdefd82271341c33e008f6
Aidan Boland <aidanboland1@icloud.com>
2022-11-25 12:44:01 +0000

    fDF

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -7,6 +7,11 @@ import Dropdown from '../Dropdown/Dropdown';
 function Card() {
     return (
         <div className="cardMain">
+            <div className="SVGs">
+                <svg viewBox='0 0 100 100'>
+                    <use xlinkHref='../../assets/bg-pattern-desktop.svg'/>
+                </svg>
+            </div>
             <div className="MainTitle">
                 <h2>FAQ</h2>
             </div>
@@ -30,25 +35,6 @@ function Card() {
                 title="Do you provide additional support?"
                 content="Instant chat support is available at all times to premium members, for lower tiers there may be up to an hour wait time. Alternatively, an email can be sent to our support teams, who will get back to you as soon as possible."
             />
-            {/* <div className="SVGs">
-                <svg width="100%" xmlns="http://www.w3.org/2000/svg">
-                    <g fillRule="nonzero" fill="none" opacity=".099">
-                        <path
-                            d="M468.755 937.353L19.744 671.73c-14.295-8.047-26.548-21.23-12.62-29.878L457.734 381.89c6.754-4.194 17.018-6.946 23.903-2.936l471.265 273.827c14.296 8.308 16.861 31.45 2.619 39.837L527.925 936.933a59.64 59.64 0 01-59.17.42z"
-                            fill="#6862E6"
-                        />
-                        <path
-                            d="M468.755 561.353L19.744 295.73c-14.295-8.047-26.548-21.23-12.62-29.878L457.734 5.89c6.754-4.194 17.018-6.946 23.903-2.936l471.265 273.827c14.296 8.308 16.861 31.45 2.619 39.837L527.925 560.933a59.64 59.64 0 01-59.17.42z"
-                            stroke="#1E1F36"
-                        />
-                        <path
-                            d="M468.755 636.353L19.744 370.73c-14.295-8.047-26.548-21.23-12.62-29.878L457.734 80.89c6.754-4.194 17.018-6.946 23.903-2.936l471.265 273.827c14.296 8.308 16.861 31.45 2.619 39.837L527.925 635.933a59.64 59.64 0 01-59.17.42z"
-                            stroke="#1E1F36"
-                        />
-                    </g>
-                </svg>
-                <img src='../../assets/bg-pattern-desktop.svg' alt=''/>
-            </div> */}
         </div>
     );
 }
